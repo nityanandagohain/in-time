1d0e7a347656f3e5fb95b22252da218e7b11fe47
Satkriti-Singh <36204206+Satkriti-Singh@users.noreply.github.com>
2018-12-19 08:39:22 +0530

    Edit Drawer (#19)
    * Log Out from Drawer

--- lib/screens/drawer.dart
+++ lib/screens/drawer.dart
@@ -1,61 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'homePage.dart';
 
-Widget drawerLeft() {
-    return Drawer(
-      child: ListView(
+Widget drawerLeft(BuildContext context) {
+    return Drawer (
+      child : ListView(
         // Important: Remove any padding from the ListView.
         children: <Widget>[
-          UserAccountsDrawerHeader(
+          new Container(
+            child: new UserAccountsDrawerHeader(
             accountName: Text("John Doe"),
             accountEmail: Text("johndoe@gmail.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.orangeAccent,
-              child: Text("JD",style: TextStyle(color: Colors.white),),
+            decoration: new BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: new AssetImage("assets/img/bg.png")
+              )
             ),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: new AssetImage("assets/img/mainpic.png"),
+            ),
+            otherAccountsPictures: <Widget>[
+              new GestureDetector(
+                child: new CircleAvatar(
+                  backgroundImage: new AssetImage("assets/img/otherpic.png"),
+                ),
+              )
+            ],
           ),
-          ListTile(
+      ),
+      new Container(
+        color: Colors.yellow[900],
+          child : ListTile(
             title: Text('Home'),
             onTap: () {
-
+              Navigator.pop(context);
             },
             leading: Icon(Icons.home),
           ),
-          ListTile(
+          ),
+      new Container(
+          color: Colors.yellow[800],
+          child : ListTile(
             title: Text('Subjects'),
             onTap: () {
               // Update the state of the app
               // ...
+              Navigator.pop(context);
             },
             leading: Icon(Icons.info_outline),
           ),
-          ListTile(
+          ),
+          new Container(
+          color: Colors.yellow[700],
+          child :ListTile(
             title: Text('Pending Tasks'),
             onTap: () {
               // Update the state of the app
               // ...
+              Navigator.pop(context);
             },
             leading: Icon(Icons.apps),
           ),
-          ListTile(
-            title: Text('Setting'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-            },
-            leading: Icon(Icons.settings),),
-            ListTile(
-            title: Text('Logout'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-            },
-            leading: Icon(Icons.add_circle_outline),
-
           ),
-
+          new Container(
+            color: Colors.yellow[600],
+          child :ListTile(
+            title: Text('Settings'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              Navigator.pop(context);
+            },
+            leading: Icon(Icons.settings),
+          ),
+          ),
+          new Container(
+          color: Colors.yellow[500],
+          child :ListTile(
+            title: Text('Signout'),
+            onTap: () {
+              FirebaseAuth.instance.signOut().then((value){
+                          Navigator.of(context).pushReplacementNamed('landingpage');
+                        })
+                        .catchError((e){
+                          print(e);
+                        });
+              Navigator.pop(context);
+            },
+            leading: Icon(Icons.lock_open),
+          ),
+          ),
         ],
       ),
     );
   }
+  
--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -110,23 +110,12 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      drawer: drawerLeft(),
+      drawer: drawerLeft(context),
       appBar: AppBar(
           title: Text(
             "IN TIME",
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
           ),
-         actions: <Widget>[
-          PopupMenuButton<String>(
-           
-            itemBuilder: (BuildContext context){
-              return Constants.choices.map((String choice){
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();},
-               onSelected: choiceAction,),],
           backgroundColor: clr,
           elevation: 0.0,
           leading: MaterialButton(
@@ -237,15 +226,5 @@ class _HomePageState extends State<HomePage>
     );
 
   }
-   void choiceAction(String choice){
-     
-    
-      FirebaseAuth.instance.signOut().then((value){
-                          Navigator.of(context).pushReplacementNamed('landingpage');
-                        })
-                        .catchError((e){
-                          print(e);
-                        });
-    
-  }
+   
 }

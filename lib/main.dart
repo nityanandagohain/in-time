daae3b314329cf146e2894a7968732fbf2cc60de
BHAVNA HARITSA <bhavnaharitsa@gmail.com>
2018-12-31 10:23:40 +0530

    added the proper code for configuring the push up notifs

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:in_time/screens/signuppage.dart';
 import 'package:in_time/utils/custom_loader.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+
 
 
 
@@ -14,9 +17,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
 String text="Hello World";
 FirebaseMessaging fb = new FirebaseMessaging();
+FlutterLocalNotificationsPlugin fl= new FlutterLocalNotificationsPlugin();
 
 @override
 void initstate(){
+  super.initstate();
+  var android= new AndroidInitializationSettings('minmap/iclauncher');
+  var ios = new IosNotificationSettings();
+  var platform = new InitializationSettings(android, ios);
+  fl.initialize(platform);
+
   fb.configure(
     onLaunch: (Map<String,dynamic> msg){
       print(" on Launch called");
@@ -25,6 +35,8 @@ void initstate(){
       print("called");
     },
     onMessage: (Map<String,dynamic> msg){
+      showmessage(msg);
+
       print(" on Resume called ");
 
     }
@@ -36,8 +48,32 @@ void initstate(){
       alert: true,
     )
   );
+  fb.onIosSettingsRegistered.listen((IosNotificationSettings setting){
+    print("IOS setting registered");
+  });
+  fb.getToken().then((token){
+    update(token);
+  });
+}
+showmessage(Map<String , dynamic> msg) async{
+  var android = new AndroidNotificationDetails('channel_id',"Channel Name","Channel Description");
+  var ios = new IOSNotificationDetails();
+  var platform = new NotificationDetails(android, ios);
+ FlutterLocalNotificationsPlugin.show(0,"This is title","This is demo",platform);
+
+}
+update(String token){
+  print(token);
+  text=token;
+  setState((){
+
+
+  });
+
 }
   @override
+
+
 
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,4 +87,4 @@ void initstate(){
         '/homepage':(BuildContext context)=> new HomePage()
       });
   }
-}
+

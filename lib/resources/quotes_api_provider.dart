213bbea71a092a6f28ea4e992575950490d52fd2
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-01-03 05:45:24 +0530

    Fix quotes situtation (#44)
    * Fix show quote on all screens
    
    * Fix: Show different quote every day

--- lib/resources/quotes_api_provider.dart
+++ lib/resources/quotes_api_provider.dart
@@ -2,20 +2,21 @@ import 'dart:async';
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import 'package:in_time/models/quotes_model.dart';
+import 'package:in_time/models/quotes_model_new.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class QuotesApiProvider {
-  Client client = Client();
-//  final _apiKey = 'your_api_key';
-
-  Future<QuotesModel> fetchMovieList() async {
-    print("entered");
-    var uri = "https://gist.githubusercontent.com/knightcube/48ee2da4221303a22dabd0c99daa317b/raw/94712c18af649c22bea89255404afc92494998c9/quotes.json";
+  Future<Q> fetchMovieList() async {
+    print("entered 2");
+    // var uri = "https://gist.githubusercontent.com/knightcube/48ee2da4221303a22dabd0c99daa317b/raw/94712c18af649c22bea89255404afc92494998c9/quotes.json";
     var uri2 = "http://quotes.rest/qod.json?category=inspire";
-    final response = await client.get(Uri.encodeFull(uri),headers: {"Accept": "application/json"} );
+    var response = await http.get(uri2);
     print(response.body.toString());
+
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
-      return QuotesModel.fromJson(json.decode(response.body));
+      return Q.fromJson(json.decode(response.body));
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');

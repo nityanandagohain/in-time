416982849542697713990feee9875575c73fff50
BHAVNA HARITSA <bhavnaharitsa@gmail.com>
2019-01-06 09:09:34 +0530

    removed the tagline in the corner "debug printer"

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'In Time',
       
       home: (FirebaseAuth.instance.currentUser() == null)? LoginPage(): HomePage(),
